5d6f841f49db792dc3b74528fa9dc2877c744a83
Klaus Wenger <kjwenger@yahoo.com>
2024-12-28 14:47:12 +0100

    initial load

---
+++ TideTimes/TideTimes/TideTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesApp.swift
+//  TideTimes
+//
+//  Created by Klaus Wenger on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TideTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TideTimes/TideTimesTests/TideTimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TideTimesTests.swift
+//  TideTimesTests
+//
+//  Created by Klaus Wenger on 12/28/24.
+//
+
+import Testing
+@testable import TideTimes
+
+struct TideTimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
